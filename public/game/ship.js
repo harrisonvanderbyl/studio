c43371ac708511b075ec2f5b4695199cd0a9b11a
Samuel-Clarke123 <clarkepearl44@gmail.com>
2018-09-22 20:58:06 +1000

    added stubs

--- public/game/ship.js
+++ public/game/ship.js
@@ -1,6 +1,24 @@
 class Ship extends Actor {
     constructor(isPlayer=false) {
         super();
+        this.isPlayer = isPlayer;
     }
-    
+
+    turn(dir=-1) { // -1 for left, +1 for right.
+        //stub
+    }
+
+    boost() { // accelerates the ships velocity.
+        //stub
+    }
+
+    update() {
+        super();
+        //stub
+    }
+
+    draw() {
+        super();
+        //stub
+    }
 }
